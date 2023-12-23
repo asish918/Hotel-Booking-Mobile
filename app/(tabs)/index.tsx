2353e63a4fc5e43c457e7f165201ecc173bc9a70
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-23 10:19:27 +0530

    Added Mapview to the main page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,14 +2,14 @@ import { View } from 'react-native';
 import React, { useMemo, useState } from 'react';
 import ListingsBottomSheet from '@/components/ListingsBottomSheet';
 import listingsData from '@/assets/data/airbnb-listings.json';
-// import ListingsMap from '@/components/ListingsMap';
-// import listingsDataGeo from '@/assets/data/airbnb-listings.geo.json';
+import ListingsMap from '@/components/ListingsMap';
+import listingsDataGeo from '@/assets/data/airbnb-listings.geo.json';
 import { Stack } from 'expo-router';
 import ExploreHeader from '@/components/ExploreHeader';
 
 const Page = () => {
   const items = useMemo(() => listingsData as any, []);
-  // const getoItems = useMemo(() => listingsDataGeo, []);
+  const getoItems = useMemo(() => listingsDataGeo, []);
   const [category, setCategory] = useState<string>('Tiny homes');
 
   const onDataChanged = (category: string) => {
@@ -24,7 +24,7 @@ const Page = () => {
           header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
         }}
       />
-      {/* <ListingsMap listings={getoItems} /> */}
+      <ListingsMap listings={getoItems} />
       <ListingsBottomSheet listings={items} category={category} />
     </View>
   );
